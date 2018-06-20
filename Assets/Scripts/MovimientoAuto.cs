0a687678885356eef919e60b031eac9a6b9d912c
fam2k <facundoam94@gmail.com>
2018-06-19 21:23:04 -0300

    circuito

--- Assets/Scripts/MovimientoAuto.cs
+++ Assets/Scripts/MovimientoAuto.cs
@@ -10,7 +10,7 @@ public class MovimientoAuto : MonoBehaviour {
 	public Transform Neumatico2; 
 	public Transform Neumatico3; 
 	public Transform Neumatico4; 
-	public int desaceleracion = 150;
+	public int desaceleracion = 200;
 	private int velocidadMaxima = 150;
 
 
@@ -20,13 +20,13 @@ public class MovimientoAuto : MonoBehaviour {
 
 	// Solo se ejecuta cuando se inicia
 	void Start () {
-		transform.GetComponent<Rigidbody>().centerOfMass = new Vector3(0,-0.5f,0);	// se pone el centro de masa en el -1 de y para que no se voltee
+		transform.GetComponent<Rigidbody>().centerOfMass = new Vector3(0,-1f,0);	// se pone el centro de masa en el -1 de y para que no se voltee
 	}
 
 	void Update(){
 		//movimiento de las llantas hacia los costados
-		Neumatico1.localEulerAngles = new Vector3 (0, Llanta1.steerAngle, 0);
-		Neumatico2.localEulerAngles = new Vector3 (0, Llanta2.steerAngle, 0);
+		Neumatico1.localEulerAngles = new Vector3 (0, Llanta1.steerAngle*2, 0);
+		Neumatico2.localEulerAngles = new Vector3 (0, Llanta2.steerAngle*2, 0);
 		//rotacion de las llantas
 		Neumatico1.Rotate(new Vector3(Llanta1.rpm/60*360*Time.deltaTime/4,0,0));
 		Neumatico2.Rotate(new Vector3(Llanta1.rpm/60*360*Time.deltaTime/4,0,0));
@@ -52,8 +52,8 @@ public class MovimientoAuto : MonoBehaviour {
 		
 
 		//Rotacion del auto
-		Llanta1.steerAngle = 7 * Input.GetAxis("Horizontal");
-		Llanta2.steerAngle = 7 * Input.GetAxis("Horizontal");
+		Llanta1.steerAngle = 10 * Input.GetAxis("Horizontal");
+		Llanta2.steerAngle = 10 * Input.GetAxis("Horizontal");
 
 		//Desaceleracion
 		if (Input.GetAxis("Vertical")==0) {
