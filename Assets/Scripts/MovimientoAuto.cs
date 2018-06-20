4f4822ef5cbce85dd44fa699a74c33b08bb3cc24
JRey05 <julianrey@outlook.com>
2018-06-19 21:44:28 -0300

    script

--- Assets/Scripts/MovimientoAuto.cs
+++ Assets/Scripts/MovimientoAuto.cs
@@ -6,33 +6,46 @@ public class MovimientoAuto : MonoBehaviour {
 
 	public WheelCollider Llanta1;
 	public WheelCollider Llanta2;
+	public WheelCollider Llanta3;
+	public WheelCollider Llanta4;
 	public Transform Neumatico1;
 	public Transform Neumatico2; 
 	public Transform Neumatico3; 
 	public Transform Neumatico4; 
-	public int desaceleracion = 200;
-	private int velocidadMaxima = 150;
+	public int desaceleracion = 150;
+	private int velocidadMaxima = 300;
 
 
-	public double velocidad;
-	public int aceleracion = 300;
+	public float velocidad;
+	public int aceleracion = 100;
+	public float cuentangulo1 = 0f;
+	public float cuentangulo2 = 0f;
+	public float cuentangulo3 = 0f;
+	public float cuentangulo4 = 0f;
 
 
 	// Solo se ejecuta cuando se inicia
 	void Start () {
-		transform.GetComponent<Rigidbody>().centerOfMass = new Vector3(0,-1f,0);	// se pone el centro de masa en el -1 de y para que no se voltee
+		transform.GetComponent<Rigidbody>().centerOfMass = new Vector3(0,-0.5f,0);	// se pone el centro de masa en el -1 de y para que no se voltee
 	}
 
 	void Update(){
+		cuentangulo1 = cuentangulo1 + Llanta1.rpm/60*360*Time.deltaTime;
+		cuentangulo2 = cuentangulo2 + Llanta2.rpm/60*360*Time.deltaTime;
+		cuentangulo3 = cuentangulo3 + Llanta3.rpm/60*360*Time.deltaTime;
+		cuentangulo4 = cuentangulo4 + Llanta4.rpm/60*360*Time.deltaTime;
 		//movimiento de las llantas hacia los costados
-		Neumatico1.localEulerAngles = new Vector3 (0, Llanta1.steerAngle*2, 0);
-		Neumatico2.localEulerAngles = new Vector3 (0, Llanta2.steerAngle*2, 0);
+		Neumatico1.localEulerAngles = new Vector3 (cuentangulo1, Llanta1.steerAngle*2, 0);
+		Neumatico2.localEulerAngles = new Vector3 (cuentangulo2, Llanta2.steerAngle*2, 0);
+		Neumatico3.localEulerAngles = new Vector3 (cuentangulo3, 0, 0);
+		Neumatico4.localEulerAngles = new Vector3 (cuentangulo4, 0, 0);
 		//rotacion de las llantas
-		Neumatico1.Rotate(new Vector3(Llanta1.rpm/60*360*Time.deltaTime/4,0,0));
-		Neumatico2.Rotate(new Vector3(Llanta1.rpm/60*360*Time.deltaTime/4,0,0));
-		Neumatico3.Rotate(new Vector3(Llanta1.rpm/60*360*Time.deltaTime/4,0,0));
-		Neumatico4.Rotate(new Vector3(Llanta1.rpm/60*360*Time.deltaTime/4,0,0));
-		velocidad= (2* 3.1416 * Llanta1.radius) * (Llanta1.rpm * 60/1000);			// perimetro * rpm * conversion a km/h de m/min
+
+	//	Neumatico1.Rotate(new Vector3(cuentangulo1,0,0));
+	//	Neumatico2.Rotate(new Vector3(cuentangulo2,0,0));
+	//	Neumatico3.Rotate(new Vector3(cuentangulo3,0,0));
+	//	Neumatico4.Rotate(new Vector3(cuentangulo4,0,0));
+		velocidad= (float)(3.1416 * Llanta1.radius) * (Llanta1.rpm * 60/1000);			// perimetro * rpm * conversion a km/h de m/min
 	}
 	
 	// Se ejecuta varias veces por segundo
@@ -52,8 +65,9 @@ public class MovimientoAuto : MonoBehaviour {
 		
 
 		//Rotacion del auto
-		Llanta1.steerAngle = 10 * Input.GetAxis("Horizontal");
-		Llanta2.steerAngle = 10 * Input.GetAxis("Horizontal");
+		Llanta1.steerAngle = 7 * Input.GetAxis("Horizontal");
+		Llanta2.steerAngle = 7 * Input.GetAxis("Horizontal");
+
 
 		//Desaceleracion
 		if (Input.GetAxis("Vertical")==0) {
